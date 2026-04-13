a186d711327a7309bfcb06abc9a1e47d8540cb60
HannaYastremska <anna_yastrem@ukr.net>
2026-04-13 20:45:42 +0300

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,5 @@
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  modal: document.querySelector("[data-modal]"), // Скрипт шукає цей атрибут!
+};
